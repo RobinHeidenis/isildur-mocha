2f977cbc5cd59b8719e8dc4e0d8a14586ec4dbc4
Robin Heidenis <robin.heidenis@gmail.com>
2023-05-26 23:09:56 +0200

    fix: 🐛 options only being applied if config is defined
    ✅ Closes: #11

--- src/helpers/getOptions.ts
+++ src/helpers/getOptions.ts
@@ -1,15 +1,23 @@
+import { PartialTestRunnerOptions } from "@isildur-testing/api";
 import { loadConfigFile } from "~/helpers/loadConfigFile.js";
 
 export const getOptions = () => {
   const options = process.argv[2]
-    ? (JSON.parse(process.argv[2]) as Record<string, unknown>)
+    ? (JSON.parse(process.argv[2]) as PartialTestRunnerOptions)
     : undefined;
 
-  return options?.config
-    ? loadConfigFile(
+  return {
+    ...(options?.config &&
+      loadConfigFile(
         options?.config
           ? `--config=${(options.config as string | undefined) ?? ""}`
           : ""
-      )
-    : undefined;
+      )),
+    ...(options?.bail && { bail: options.bail }),
+    ...(options?.diff && { diff: options.diff }),
+    ...(options?.allowNoTests && { failZero: !options.allowNoTests }),
+    ...(options?.testNameFilter && { grep: options.testNameFilter }),
+    ...(options?.timeout && { timeout: options.timeout }),
+    ...(options?.maxWorkers && { jobs: options.maxWorkers }),
+  } satisfies Mocha.MochaOptions;
 };
